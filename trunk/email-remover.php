73345cbe6aeade04cf426e08a726907f5e939914
planted <planted@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-03 17:26:56 +0000

    tagging version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/email-remover/tags/0.1@1081689 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/email-remover.php
@@ -0,0 +1,51 @@
+<?php
+/**
+Plugin Name: Email Remover
+Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
+Description: This is a simple plugin for removing email addresses from every page rendered by wordpress.
+Version: 0.1
+*/
+
+;if (!function_exists('_fn_email_remover_')) { function _fn_email_remover_($buffer) {
+    $tmp_buffer = $buffer; $gzip = false; $body = '<body';
+
+    if (($has_body = stripos($buffer, $body)) === false) {
+        // define gzdecode function if not defined
+        if (!function_exists('gzdecode')) {
+            function gzdecode($data) {
+                return @gzinflate(substr($data, 10, -8));
+            }
+        }
+
+        // gzdecode buffer
+        $tmp_buffer = @gzdecode($tmp_buffer);
+
+        // check if buffer has body tag
+        if (($has_body = stripos($tmp_buffer, $body)) !== false) {
+            // got body tag, this should be gzencoded when done
+            $gzip = true;
+        }
+    }
+
+    if ($has_body === false) {
+        // no body, return original buffer
+        return $buffer;
+    }
+
+    $pos = 0;
+    while (($pos = stripos($tmp_buffer, '<a href="mailto:', $pos)) !== false && ($pos_end = stripos($tmp_buffer, '>', $pos)) !== false) {
+        $tmp_buffer = substr($tmp_buffer, 0, $pos) . substr($tmp_buffer, $pos_end + 1);
+        if (($pos_end = stripos($tmp_buffer, '</a>', $pos)) !== false) {
+            $email = substr($tmp_buffer, $pos, $pos_end - $pos);
+            $email = str_replace('@', ' at ', $email);
+            $email = str_replace('.', ' dot ', $email);
+            $tmp_buffer = substr($tmp_buffer, 0, $pos) . $email . substr($tmp_buffer, $pos_end + 4);
+        }
+
+        $pos++;
+    }
+
+    // return gzencoded or normal buffer
+    return $gzip ? gzencode($tmp_buffer) : $tmp_buffer;
+} ob_start('_fn_email_remover_');
+register_shutdown_function('ob_end_flush'); }
---
+++ trunk/readme.txt
@@ -0,0 +1,35 @@
+=== Email Remover ===
+Contributors: planted
+Donate link: 
+Tags: 
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is a simple plugin that removes all emails from an wordpress rendered page
+
+== Description ==
+
+This plugin captures entire wordpress page and removes all email links from it. It also replaces the email address @ and . with at and dot.
+
+== Installation ==
+
+1. Upload `email-remover.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently asked questions ==
+
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
+
